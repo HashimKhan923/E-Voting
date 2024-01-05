1699b5b6164642cbf554baaca9c681c4a82f02a8
HashimKhan923 <khanhash1994@gmail.com>
2024-01-05 08:23:34 +0500

    dsdsxsxsxxd

--- app/Http/Controllers/Admin/BannerController.php
+++ app/Http/Controllers/Admin/BannerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Model\Banner;
+use App\Models\Banner;
 
 class BannerController extends Controller
 {
