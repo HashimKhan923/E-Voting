c824c68c8aa8bb11faf698585ccd8ff73c1f2888
HashimKhan923 <khanhash1994@gmail.com>
2024-01-06 04:27:04 +0500

    sddsxaxd

--- routes/api.php
+++ routes/api.php
@@ -75,7 +75,7 @@ Route::group(['middleware' => ['auth:api']], function(){
 
         Route::group(['prefix' => '/admin/dashboard'], function() {
             Route::controller(App\Http\Controllers\Admin\DashboardController::class)->group(function () {
-                Route::get('show','index');
+                Route::get('','index');
             });
         });
 
