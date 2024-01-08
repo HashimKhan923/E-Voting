6aa5a2e23fdd4f1d8e94bc8f193393be1f113610
HashimKhan923 <khanhash1994@gmail.com>
2024-01-09 03:54:08 +0500

    gggg

--- app/Http/Controllers/Admin/BannerController.php
+++ app/Http/Controllers/Admin/BannerController.php
@@ -20,6 +20,9 @@ class BannerController extends Controller
 
         $new = new Banner();
 
+        $count = Banner::count();
+        $order = $count+1;
+
         if($request->file('image'))
         {
             $file= $request->image;
@@ -27,10 +30,39 @@ class BannerController extends Controller
             $file->move(public_path('Banner'),$filename);
             $new->image = $filename;
         }
+        $new->sort = $order;
         $new->link = $request->link;
         $new->save();
 
         return response()->json(['message'=>'created successfully!']);
+    }
+
+    public function update(Request $request)
+    {
+
+        $update = Banner::where('id',$request->id)->first();
+
+        if($request->order != null)
+        {
+            $changeOrder = Banner::where('sort',$request->sort)->first();
+            $changeOrder->sort = $update->sort;
+            $changeOrder->save();
+        }
+
+
+        if($request->file('image'))
+        {
+            $file= $request->image;
+            $filename= date('YmdHis').$file->getClientOriginalName();
+            $file->move(public_path('Banner'),$filename);
+            $update->image = $filename;
+        }
+
+        $update->sort = $request->sort;
+        $update->link = $request->link;
+        $update->save();
+
+        return response()->json(['message'=>'updated successfully!']);
     }
 
     public function delete($id)
