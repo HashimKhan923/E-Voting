74a55ac3de7c5ec8b62ec3039d44ffe353860d24
HashimKhan923 <khanhash1994@gmail.com>
2024-01-09 05:02:30 +0500

    sssssssssssss

--- app/Http/Controllers/Admin/YoutubeGalleryController.php
+++ app/Http/Controllers/Admin/YoutubeGalleryController.php
@@ -10,7 +10,7 @@ class YoutubeGalleryController extends Controller
 {
     public function index()
     {
-        $data = YoutubeGallery::all();
+        $data = YoutubeGallery::orderBy('sort','ASC')->get();
 
         return response()->json(['data'=>$data]);
     }
@@ -18,10 +18,36 @@ class YoutubeGalleryController extends Controller
     public function create(Request $request)
     {
         $new = new YoutubeGallery();
+
+
+        $count = YoutubeGallery::count();
+        $order = $count+1;
+
         $new->link = $request->link;
+        $new->sort = $order;
         $new->save();
 
         return response()->json(['message'=>'created successfully!']);
+    }
+
+    public function update(Request $request)
+    {
+
+        $update = YoutubeGallery::where('id',$request->id)->first();
+
+        if($request->sort != null)
+        {
+            $changeOrder = YoutubeGallery::where('sort',$request->sort)->first();
+            $changeOrder->sort = $update->sort;
+            $changeOrder->save();
+        }
+
+
+        $update->sort = $request->sort;
+        $update->link = $request->link;
+        $update->save();
+
+        return response()->json(['message'=>'updated successfully!']);
     }
 
     public function delete($id)
